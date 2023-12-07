5ae2ecea0c6d3c38570a864e96708b2c7b6eb4cd
José Victor <victor.jose.barros.fontes@gmail.com>
2023-12-06 23:58:42 -0300

    EncontreCostureiro FINLIZADO! + nova lib

--- src/pagesCliente/EncontreCostureiro.js
+++ src/pagesCliente/EncontreCostureiro.js
@@ -1,15 +1,21 @@
 import Style from "./css/EncontreCostureiro.module.css";
 import Button from "../imagens/cadastroButtons.svg";
-import Header from "../ComponentesIndex/Header";
+import HeaderL from "../pages/HeaderLogada";
 import AutoCarousel from "../Components/AutoCarousel";
-import FaceUser from "./images/FaceUser.svg";
 import Agulha from "../imagens/agulha.svg";
 import Rating from "../Components/RatingSystem";
+
+import fotoPerfil from '../imagens/fotoperfil.svg';
+import fotoPerfil2 from '../imagens/fotoperfil2.svg';
+import fotoPerfil3 from '../imagens/fotoperfil3.svg';
+import Carousel1 from '../pagesCostureiro/Componentes/Slides01';
+import Carousel2 from '../pagesCostureiro/Componentes/Slides02';
+import Carousel3 from '../pagesCostureiro/Componentes/Slides03';
 
 function EncontreCostureiro(){
     return(
         <div>
-            <Header />
+            <HeaderL />
             <div className={Style.block}></div>
             <AutoCarousel />
 
@@ -19,14 +25,46 @@ function EncontreCostureiro(){
                 <img src={Button} alt='botões' />
             </div>
 
-    <div className={Style.main}>
-            <div class={Style.wrapper}>
-                <div class={Style.icon}></div>
-                 <p class={Style.input}>@PIOLLI</p>
-                 <div class={Style.icon2}></div>
-            </div>  
-                <Rating />
-        </div>
+
+            <section className={Style.conteudoPedidos}>
+
+                <div className={Style.carrousels}>
+
+                    <div className={Style.carrousel}>
+                        <span className={Style.perfil}>
+                            <img src={fotoPerfil} alt='Imagem perfil. Pessoa Branca' />
+                            <p>@ARAUJO</p>
+                            <img className={Style.imgPlano} src={Agulha} alt='Agulha, simbolo da assinatura' />
+                        </span>
+                        <Rating />
+                        <Carousel1 />
+                    </div>
+
+                    <div className={Style.carrousel}>
+                        <span className={Style.perfil}>
+                            <img src={fotoPerfil3} alt='Imagem perfil. Pessoa Branca' />
+                            <p>@MELLINHO</p>
+                        </span>
+                        <Rating />
+                        <Carousel2 />
+                    </div>
+
+                    <div className={Style.carrousel}>
+                        <span className={Style.perfil}>
+                            <img src={fotoPerfil2} alt='Imagem perfil. Pessoa Branca' />
+                            <p>@BIOCAS</p>
+                        </span>
+                        <Rating />
+                        <Carousel3 />
+                    </div>
+
+
+                </div>
+
+            </section>
+      
+                
+
 
     </div>
     );
