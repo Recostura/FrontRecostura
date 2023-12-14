54cf84307b787fa14cd48a3242298d0c2e9c585a
Guilherme Araujo <134068310+GuilhermeAraujo-C@users.noreply.github.com>
2023-12-13 23:26:38 -0300

    modal estilizada

--- src/pages/Componentes/SaldoBonus_Modal.js
+++ src/pages/Componentes/SaldoBonus_Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Style from './css/SaldoBonus_Modal.module.css';
 import info from '../../imagens/interrogração.svg';
+import Fechar from '../../imagens/modalCostureiroFechar.svg';
 
 
 const SaldoBonus = () => {
@@ -28,9 +29,13 @@ const SaldoBonus = () => {
             {modalAberto && (
                 <div className={Style.modalOverlay} onClick={fecharModal}>
                     <div className={Style.modalContent} onClick={(e) => e.stopPropagation()}>
-                        {/* Conteúdo do seu modal aqui */}
-                        <p>Seu conteúdo modal vai aqui.</p>
-                        <button onClick={fecharModal}>Fechar</button>
+                       <div className={Style.textModal}>
+                            <h2>Saldo Bonus</h2>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                       </div>
+                        <button onClick={fecharModal} className={Style.buttonModal}>
+                            <img src={Fechar} alt='fechar' />
+                        </button>
                     </div>
                 </div>
             )}
